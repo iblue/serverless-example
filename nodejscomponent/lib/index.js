a0158ec3681dbad4f2f7e136b319c1f5f3cb1b1a
Markus Fenske <iblue@gmx.net>
2016-01-19 16:19:41 +0100

    Add DynamoDB

--- nodejscomponent/lib/index.js
+++ nodejscomponent/lib/index.js
@@ -2,6 +2,25 @@
  * Lib
  */
 
+/* Init DynamoDB stuff */
+var AWS = require('aws-sdk');
+var Saws = new require('saws')(AWS);
+
+AWS.config.update({region:'eu-west-1'});
+Saws.stage = process.env.SERVERLESS_STAGE;
+var RESOURCES_PARAMS = {
+  TableName: "resources",
+  KeySchema: [
+    {AttributeName: 'Name', KeyType: 'HASH'},
+  ],
+  AttributeDefinitions: [
+    {AttributeName: 'Name', AttributeType: 'S'}
+  ],
+  ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+};
+
+module.exports.resources = new Saws.Table(RESOURCES_PARAMS);
+
 module.exports.respond = function(event, cb) {
 
   var response = {
--- nodejscomponent/resource/show/handler.js
+++ nodejscomponent/resource/show/handler.js
@@ -16,7 +16,10 @@ var lib = require('../../lib');
 // Lambda Handler
 module.exports.handler = function(event, context) {
 
-  lib.respond(event, function(error, response) {
+  lib.resources.save({
+    "Name": event["name"]
+  }, function(error, response) {
     return context.done(error, response);
   });
+
 };
